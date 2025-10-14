2e4df723bbd96890d7abad31429ae8fd7b38781d
MADANA-GS <madangsnaik@gmail.com>
2025-10-15 00:08:29 +0530

    Updated the auth call back ui

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-const Loader = () => {
+const Loader = ({ clr = "white" }) => {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <div className="w-4 h-4 border-[2px] border-white border-t-transparent rounded-full animate-spin"></div>
+      <div
+        className={`w-4 h-4 border-[2px] ${
+          clr == "black" ? "border-black" : "border-white"
+        } border-t-transparent rounded-full animate-spin`}
+      ></div>
     </div>
   );
 };
--- client/src/components/callback/AuthCallback.jsx
+++ client/src/components/callback/AuthCallback.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Loader from "../Loader";
 
 export default function AuthCallback() {
   const navigate = useNavigate();
@@ -25,9 +26,10 @@ export default function AuthCallback() {
   }, [navigate]);
 
   return (
-    <div>
-      <h1 className="text-black bg-white px-2 py-2">
-        Logging in with GitHub...
+    <div className=" flex items-center justify-center h-screen w-screen">
+      <h1 className="text-black items-center justify-center bg-gray-200 px-2 flex gap-2 py-2">
+        <p className="flex-shrink-0"> Logging in with GitHub... </p>
+        <Loader clr="black" />
       </h1>
     </div>
   );
